1968e709c620954ce33ddc09478defd44e3251ce
Muhammad Abdullah Nadeem <mabdullahnadeem313@gmail.com>
2024-01-26 07:10:26 +0000

    Quantity validations added

--- srv/wholefoods-service.js
+++ srv/wholefoods-service.js
@@ -8,38 +8,93 @@ module.exports = async function (srv) {
 
   let PO_HEADER_EBELN = "0";
   // let PO_ITEM_EBELP = '0';
-  let PO_ITEM_EBELP = "0";
   let ITEMID = "0";
+  let PO_ITEM_EBELP = "0";
 
+  this.before('*',async req=>{
+    if (req.target){
+      let entityName = req.target.name;
+      console.log("calling entity name:  " + entityName);
+    }
+    if (req.event) {
+      let methodName = req.event;
+      console.log("calling method name: " + methodName);
+    }
+  })
+
+  
   this.before("CREATE", "PO_Head", async (req) => {
     PO_HEADER_EBELN = parseInt(PO_HEADER_EBELN);
-
+    
     PO_HEADER_EBELN += 1;
-
+    
     req.data.EBELN = zeroPad(PO_HEADER_EBELN, 10);
-
+    
     // console.log("EBELN ###"+ req.data.EBELN);
   });
-
+  
   this.before("CREATE", "PO_Item.drafts", (req) => {
-    console.log("adding materials @@@ ");
-    ITEMID = parseInt(ITEMID);
-    ITEMID = ITEMID + 10;
-    let temp = String(ITEMID).padStart(4, "0");
-    req.data.EBELP = temp;
+      
+      console.log("adding materials @@@ ");
+      ITEMID = parseInt(ITEMID);
+      ITEMID = ITEMID + 10;
+      PO_ITEM_EBELP = String(ITEMID).padStart(4, "0");
+  
+      req.data.EBELP = PO_ITEM_EBELP;
+
+
+    
   });
+
 
   this.before("UPDATE", "PO_Item.drafts", (req) => {
-    if (req.data) {
-      if (req.data.MENGE) {
-        if (req.data.MENGE < 1) {
-          return req.error(409, `Quantity entered should be greater than zero`);
-        } else if (req.data.MENGE > 1000) {
-          return req.error(409, `Quantity entered should be less than 1000`);
-        }
-      }
+    const { data } = req;
+
+  // Check MENGE is present in the request data
+  if (data.MENGE !== undefined) {
+    const MENGEValue = data.MENGE;
+
+
+    if (MENGEValue == 0) {
+      req.error({
+        message: "Quantity value should be greater than zero.",
+        target: "MENGE"
+      });
+    }
+    else if (MENGEValue < 0) {
+      req.error({
+        message: "Negative numbers are not allowed.",
+        target: "MENGE"
+      });
+    }
+    else if (MENGEValue > 1000) {
+      req.error({
+        message: "Quantity value should not exceed 1000.",
+        target: "MENGE"
+      });
+    }
+  }
+  });
+
+
+  this.on('error',async (err) =>{
+        console.log("err ***",err);
+    if (err.element === 'EBELP') {
+        return err.message = (`Error in ${err.value} item: Only 10 items are allowed in one purchase order`);
     }
   });
+
+    // if(err.element === 'MENGE'){
+    //   if(err.value === 0){
+    //     return err.message = (err,`zero is not allowed!`);
+    //   }else if(err.value > 1000){
+    //     return err.message = (`Quantity greater than 1000 is not allowed!`);
+    //   }else{
+    //     return err.message = (`Negative nuumbers are not allowed!`);
+    //   }
+    // }
+
+
 
   // this.on('READ', 'Mard', req=>{
   //     let poitems = SELECT.from `PO_Item` .columns (b => {b.ID , b.EBELN , b.EBELP , b.WERKS,b.MATNR, b.MENGE,b.UOM})
